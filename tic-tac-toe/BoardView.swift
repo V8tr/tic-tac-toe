e62c894fc00189b5fbe627ec31e78564b8a30a53
Vadim Bulavin <vadim.bulavin@dev-pro.net>
2016-08-25 18:36:21 +0300

    Trying to use stack view for drawing grid

--- tic-tac-toe/BoardView.swift
+++ tic-tac-toe/BoardView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class BoardView : UIView {
     private let viewModel: BoardViewModel!
@@ -22,15 +23,49 @@ class BoardView : UIView {
         drawBoard()
     }
     
-    func drawBoard() {        
+    func drawBoard() {
+        let boardStack = UIStackView()
+        boardStack.distribution  = UIStackViewDistribution.EqualSpacing
+        var rowStacks: [UIStackView] = []
+
         for row in 0..<viewModel.rows {
+            let rowStack = UIStackView()
+            rowStack.distribution  = UIStackViewDistribution.EqualSpacing
+            var rowCellViews: [CellView] = []
             for col in 0..<viewModel.cols {
-                let cellViewModel = self.viewModel.cellViewModelAtRow(row, col: col)
+                let cellViewModel = viewModel.cellViewModelAtRow(row, col: col)
                 let cellView = CellView(viewModel: cellViewModel)
-                print("draw [\(row), \(col)]")
+                cellView.backgroundColor = UIColor(red: CGFloat.random, green: CGFloat.random, blue: CGFloat.random, alpha: 1.0)
+                
+                rowCellViews.append(cellView)
+//                self.addSubview(cellView)
+//                cellView.snp_makeConstraints(closure: { (make) in
+//                    make.width.equalTo(self).dividedBy(viewModel.cols)
+//                    make.height.equalTo(self).dividedBy(viewModel.rows)
+//                    make.left.equalTo(self).offset(self.bounds.size.width / CGFloat(col + 1))
+//                    make.top.equalTo(self).dividedBy(self.bounds.size.height / CGFloat(row + 1))
+//                })
             }
+
+            for cellView in rowCellViews {
+                rowStack.addArrangedSubview(cellView)
+            }
+//            addSubview(rowStack)
+            rowStacks.append(rowStack)
+        }
+        
+        for stack in rowStacks {
+            boardStack.addArrangedSubview(stack)
+        }
+        addSubview(boardStack)
+        boardStack.snp_makeConstraints { (make) in
+            make.size.equalTo(self)
+            make.center.equalTo(self)
         }
     }
     
-    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
 }
